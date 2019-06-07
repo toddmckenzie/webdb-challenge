c77002059abda7599e81bbe460ada5620698ce32
Todd McKenzie <toddmckenzie@Todds-MacBook-Air.local>
2019-06-07 12:36:17 -0500

    changes to get file

--- routes/projectsRouter.js
+++ routes/projectsRouter.js
@@ -27,18 +27,20 @@ router.post('/', (req, res) => {
             res.status(500).json(error)
         })
 })
+
 //working
 router.get('/', (req, res) => {
-    db('actions')
-    .select('*')
-    .join('projects', { "projects.id": "actions.projects_id"})
+    db('projects')
+    .join('actions', { "projects.id": "actions.projects_id"})
+    .select('projects.id as id', 'projects.name as name', 'projects.ProjectDescription as description', 'projects.completed as completed', 'actions.id as id', 'actions.WhatNeedsDone as description', 'actions.AddtionalNotes as notes', 'actions.Completed as completed')
         .then(result => {
-            res.status(201).json(result)
+            res.status(200).json(result)
         })
         .catch(error => {
             console.error(error)
             res.status(500).json(error)
         })
 })
+
 
 module.exports = router;
