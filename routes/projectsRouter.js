b5f921dfb6d149fc16c1be84242227591b2b4862
Todd McKenzie <toddmckenzie@Todds-MacBook-Air.local>
2019-06-08 15:28:24 -0500

    get by id with actions working

--- routes/projectsRouter.js
+++ routes/projectsRouter.js
@@ -28,19 +28,50 @@ router.post('/', (req, res) => {
         })
 })
 
+/*
 //working
 router.get('/', (req, res) => {
     db('projects')
-    .join('actions', { "projects.id": "actions.projects_id"})
-    .select('projects.id as ProjectId','projects.name as name', 'projects.ProjectDescription as description', 'projects.completed as completed', 'actions.id as id', 'actions.WhatNeedsDone as description', 'actions.AddtionalNotes as notes', 'actions.Completed as completed')
+        .then(results => {
+            for (let i = 0; i < results.length; i++){
+                console.log(results[i])
+                const id = results[i].id
+                db('actions')
+                .where({ projects_id: id })
+                .then(results => {
+                    res.status(200).json({ ...results[i], actions: results })
+                    })
+                 .catch(err => res.status(500).json({message: 'error'}))
+            }
+        })
+        .catch(error => {
+            console.error(error)
+            res.status(500).json(error)
+        })
+    })
+
+*/
+
+router.get('/:id', (req, res) => {
+    db('projects')
+        .where({ id: req.params.id })
         .then(result => {
-            res.status(200).json(result)
+            db('actions')
+            .where({ projects_id: req.params.id })
+            .then(results => {
+
+                res.json({...result[0], actions: results })
+            })
+            .catch(err => {
+                res.status(500).json({ message: 'err'})
+            })
         })
         .catch(error => {
             console.error(error)
             res.status(500).json(error)
         })
 })
+
 
 
 module.exports = router;
